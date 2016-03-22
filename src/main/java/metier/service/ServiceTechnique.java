32a21d25d2eb47e7c8053b40d490adc7bc91befb
alepic <adrien.lepic@insa-lyon.fr>
2016-03-22 17:45:52 +0100

    methode nerest Livreure

--- src/main/java/metier/service/ServiceTechnique.java
+++ src/main/java/metier/service/ServiceTechnique.java
@@ -5,6 +5,16 @@
  */
 package metier.service;
 
+import com.google.maps.GeoApiContext;
+import com.google.maps.model.LatLng;
+import java.util.List;
+import metier.modele.Drone;
+import metier.modele.Livreur;
+import metier.modele.LivreurCycliste;
+import util.GeoTest;
+import static util.GeoTest.getFlightDistanceInKm;
+import static util.GeoTest.getTripDurationByBicycleInMinute;
+
 /**
  *
  * @author quentinvecchio
@@ -16,5 +26,41 @@ public class ServiceTechnique {
         System.out.println("Dest : " + destinataire);
         System.out.println("Objet : " + objet);
         System.out.println(corps);
+    }
+    public Livreur findeNirestLivrer (List <Livreur> livreurs , Double longitude, Double latitude){
+        Double TimeMin = Double.MAX_VALUE;
+        Livreur nirestLivrer = null; 
+        Double curentTime; 
+        for (Livreur curenteLivrere : livreurs){
+            LatLng[] step = new LatLng[0];
+            LatLng livreurPostion  = new LatLng(curenteLivrere.getLatitude(),curenteLivrere.getLatitude());
+            
+            // livreur est un done 
+            if (Drone.class == curenteLivrere.getClass()){
+                Drone curDrone = (Drone) curenteLivrere;
+                curentTime = getFlightDistanceInKm(livreurPostion , new LatLng(latitude,longitude) )
+                        *curDrone.getAvrSpeed();
+            }
+            
+            // livreur est un Cycliste
+            else  if (LivreurCycliste.class == curenteLivrere.getClass()){
+            curentTime = getTripDurationByBicycleInMinute(livreurPostion ,new LatLng(latitude,longitude), step );
+            
+            } 
+            
+            //en cas d'érreurs 
+            else {
+                System.out.println("érreur : ServiseTeniques.findeNirestLivrer a revoire "); //TODO sup 
+                        curentTime = Double.MAX_VALUE;
+            }
+            
+            //recherche max Val : 
+            if (curentTime<TimeMin){
+                TimeMin = curentTime; 
+                nirestLivrer = curenteLivrere; 
+            }
+        }
+        
+        return nirestLivrer; 
     }
 }
