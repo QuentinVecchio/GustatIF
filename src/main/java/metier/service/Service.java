cbc45492ae7e6b957e52e4b8da88532873472606
alepic <adrien.lepic@insa-lyon.fr>
2016-03-22 14:54:01 +0100

    import Livreur dans la classe Service

--- src/main/java/metier/service/Service.java
+++ src/main/java/metier/service/Service.java
@@ -14,6 +14,7 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import metier.modele.Client;
 import metier.modele.Commande;
+import metier.modele.Livreur;
 import metier.modele.Produit;
 import metier.modele.Restaurant;
 
